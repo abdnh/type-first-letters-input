fcd01b62cf7bd708f47926171e8d600fb5a3157a
Abdo <abd.nh25@gmail.com>
2022-08-25 12:55:32 +0300

    Add verbatim input mode
    Also make first-letters input contenteditable and handle the input event instead of keydown to work around event being stolen in Anki by shortcuts.

--- demo.js
+++ demo.js
@@ -1,18 +1,26 @@
-function cleanSavedLetters(words, offset, i) {
+// https://stackoverflow.com/a/3866442/19396312
+function setEndOfContenteditable(contentEditableElement) {
+    var range, selection;
+    range = document.createRange();//Create a range (a range is a like the selection but invisible)
+    range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+    selection = window.getSelection();//get the selection object (allows you to change selection)
+    selection.removeAllRanges();//remove any selections already made
+    selection.addRange(range);//make the range you have just created the visible selection
+}
+
+function cleanSavedLetters(offset, i) {
     for (let j = offset; j < inputFirstLetters[i].length; j++) {
         inputFirstLetters[i][j] = null;
     }
 }
 
-function keydownHandler(i) {
+function firstLettersInputHandler(i) {
     return (e) => {
-        if (e.key.length > 1) return;
-        e.preventDefault();
-        e.stopPropagation();
-        const text = e.target.textContent + ` ${e.key}`;
+        const text = e.target.textContent;
         const words = text.split(/\s+/).filter(w => w);
         if (!words[words.length - 1]) {
-            cleanSavedLetters(words, 0, i);
+            cleanSavedLetters(0, i);
             return;
         }
         e.target.innerHTML = '';
@@ -34,21 +42,52 @@ function keydownHandler(i) {
             span.innerHTML = `${answerWords[j] ? answerWords[j] : firstLetter}&nbsp;`;
             e.target.appendChild(span);
         }
-        cleanSavedLetters(words, words.length, i);
+        cleanSavedLetters(words.length, i);
+        setEndOfContenteditable(e.target);
     };
 }
 
+function verbatimInputHandler() {
+    return (e) => {
+        const answer = e.target.parentElement.dataset.answer;
+        let text = e.target.textContent;
+        text = text.replaceAll('\u00A0', ' ');
+        console.log('text', text);
+        e.target.innerHTML = '';
+        for (const [i, c] of Object.entries(text)) {
+            let color;
+            if (answer[i] && answer[i].toLowerCase() == c.toLowerCase()) {
+                color = 'green';
+            } else {
+                color = 'red';
+            }
+            const span = document.createElement("span");
+            span.style.backgroundColor = color;
+            let d = answer[i] ? answer[i] : c;
+            if (c === ' ') {
+                d = '&nbsp;';
+            }
+            span.innerHTML = d;
+            e.target.appendChild(span);
+        }
+        setEndOfContenteditable(e.target);
+    };
+}
 
-const elements = Array.from(document.querySelectorAll(".type-first-letters"));
+const elements = Array.from(document.querySelectorAll(".typebox"));
 const inputFirstLetters = [];
 for (const [i, element] of Object.entries(elements)) {
     const input = document.createElement("div");
-    input.tabIndex = '0';
+    input.contentEditable = 'true';
     const clearButton = document.createElement("button");
     clearButton.textContent = "Clear";
     element.append(input, clearButton);
     inputFirstLetters[i] = [];
-    input.addEventListener("keydown", keydownHandler(i));
+    if (element.dataset.type.toLowerCase() === "first-letters") {
+        input.addEventListener("input", firstLettersInputHandler(i));
+    } else {
+        input.addEventListener("input", verbatimInputHandler());
+    }
     clearButton.addEventListener("click", ((i) => {
         return (e) => {
             e.target.previousElementSibling.textContent = '';
