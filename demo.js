35b6e175a808780e72f242b0f048395630988dd7
Abdo <abd.nh25@gmail.com>
2022-08-25 14:02:56 +0300

    Use a single array for user input and clean up a bit

--- demo.js
+++ demo.js
@@ -1,35 +1,30 @@
 // https://stackoverflow.com/a/3866442/19396312
 function setEndOfContenteditable(contentEditableElement) {
     var range, selection;
-    range = document.createRange();//Create a range (a range is a like the selection but invisible)
-    range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
-    range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-    selection = window.getSelection();//get the selection object (allows you to change selection)
-    selection.removeAllRanges();//remove any selections already made
-    selection.addRange(range);//make the range you have just created the visible selection
+    range = document.createRange();
+    range.selectNodeContents(contentEditableElement);
+    range.collapse(false);
+    selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
 }
 
-function cleanSavedLetters(offset, i) {
-    inputFirstLetters[i] = inputFirstLetters[i].slice(0, offset);
+function clearUserInput(i, offset) {
+    userInput[i] = userInput[i].slice(0, offset);
 }
 
 function firstLettersInputHandler(i) {
     return (e) => {
         const text = e.target.textContent;
         const words = text.split(/\s+/).filter(w => w);
-        if (!words[words.length - 1]) {
-            // Clear all saved letters when input is cleared
-            cleanSavedLetters(0, i);
-            return;
-        }
         e.target.innerHTML = '';
         const answerWords = e.target.parentElement.dataset.answer.split(" ");
         for (const [j, word] of Object.entries(words)) {
             let color;
-            let firstLetter = inputFirstLetters[i][j];
+            let firstLetter = userInput[i][j];
             if (!firstLetter) {
                 firstLetter = word[0];
-                inputFirstLetters[i][j] = firstLetter;
+                userInput[i][j] = firstLetter;
             }
             if (answerWords[j] && answerWords[j][0].toLowerCase().startsWith(firstLetter.toLowerCase())) {
                 color = 'green';
@@ -41,13 +36,9 @@ function firstLettersInputHandler(i) {
             span.innerHTML = `${answerWords[j] ? answerWords[j] : firstLetter}&nbsp;`;
             e.target.appendChild(span);
         }
-        cleanSavedLetters(words.length, i);
+        clearUserInput(i, words.length);
         setEndOfContenteditable(e.target);
     };
-}
-
-function cleanSavedVerbatimValues(offset, i) {
-    verbatimInputValues[i] = verbatimInputValues[i].slice(0, offset);
 }
 
 function verbatimInputHandler(i) {
@@ -58,10 +49,10 @@ function verbatimInputHandler(i) {
         e.target.innerHTML = '';
         for (const [j, c] of Object.entries(text)) {
             let color;
-            let currentLetter = verbatimInputValues[i][j];
+            let currentLetter = userInput[i][j];
             if (!currentLetter) {
                 currentLetter = c;
-                verbatimInputValues[i][j] = currentLetter;
+                userInput[i][j] = currentLetter;
             }
             if (answer[j] && answer[j].toLowerCase() == currentLetter.toLowerCase()) {
                 color = 'green';
@@ -77,22 +68,20 @@ function verbatimInputHandler(i) {
             span.innerHTML = d;
             e.target.appendChild(span);
         }
-        cleanSavedVerbatimValues(text.length, i);
+        clearUserInput(i, text.length);
         setEndOfContenteditable(e.target);
     };
 }
 
 const elements = Array.from(document.querySelectorAll(".typebox"));
-const inputFirstLetters = [];
-const verbatimInputValues = [];
+const userInput = [];
 for (const [i, element] of Object.entries(elements)) {
     const input = document.createElement("div");
     input.contentEditable = 'true';
     const clearButton = document.createElement("button");
     clearButton.textContent = "Clear";
     element.append(input, clearButton);
-    inputFirstLetters[i] = [];
-    verbatimInputValues[i] = [];
+    userInput[i] = [];
     if (element.dataset.type.toLowerCase() === "first-letters") {
         input.addEventListener("input", firstLettersInputHandler(i));
     } else {
@@ -101,8 +90,7 @@ for (const [i, element] of Object.entries(elements)) {
     clearButton.addEventListener("click", ((i) => {
         return (e) => {
             e.target.previousElementSibling.textContent = '';
-            inputFirstLetters[i] = [];
-            verbatimInputValues[i] = [];
+            userInput[i] = [];
         }
     })(i));
 }
